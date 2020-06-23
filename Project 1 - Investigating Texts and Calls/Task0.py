9ef40fdb5ed1efd78718456c8dead2469fe6e796
Emmanuel Aboah Boateng <57315001+aboahemmanuel01@users.noreply.github.com>
2020-06-23 14:35:23 -0200

    Update Task0.py
    Updated Task0.py

--- Project 1 - Investigating Texts and Calls/Task0.py
+++ Project 1 - Investigating Texts and Calls/Task0.py
@@ -18,6 +18,7 @@ What is the first record of texts and what is the last record of calls?
 
 Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
+
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 print("First record of texts, {0} texts {1} at time {2}".format(*texts[0]))
