2c0c6d72007de57f77f7f6f99bd151a459bc8b91
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2020-11-19 05:49:10 -0600

    Update Detecting Loops.py

--- Solved Problems/Data structures/Linked List/Detecting Loops.py
+++ Solved Problems/Data structures/Linked List/Detecting Loops.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 # # Detecting Loops in Linked Lists
-# In this notebook, you'll implement a function that detects if a loop exists in a linked list. The way we'll do this is by having two pointers, called "runners", moving through the list at different rates. Typically we have a "slow" runner which moves at one node per step and a "fast" runner that moves at two nodes per step.
-# If a loop exists in the list, the fast runner will eventually move behind the slow runner as it moves to the beginning of the loop. Eventually it will catch up to the slow runner and both runners will be pointing to the same node at the same time. If this happens then you know there is a loop in the linked list. Below is an example where we have a slow runner (the green arrow) and a fast runner (the red arrow).
+# In this notebook, you'll implement a function that detects if a loop exists in a linked list. 
+# The way we'll do this is by having two pointers, called "runners", moving through the list at different rates. 
+# Typically we have a "slow" runner which moves at one node per step and a "fast" runner that moves at two nodes per step.
+# If a loop exists in the list, the fast runner will eventually move behind the slow runner as it moves to the beginning of the loop. 
+# Eventually it will catch up to the slow runner and both runners will be pointing to the same node at the same time. 
+# If this happens then you know there is a loop in the linked list. Below is an example where we have a slow runner (the green arrow) and a fast runner (the red arrow).
 
 class Node:
     def __init__(self, value):
