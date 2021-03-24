9a2385cd90d87671186f2fbd6597aba335229522
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2021-03-24 04:02:27 -0500

    Update Add_one_2.py

--- Solved Problems/Data structures/arrays/Add_one_2.py
+++ Solved Problems/Data structures/arrays/Add_one_2.py
@@ -1,1 +1,19 @@
 # This is an alternative solution to the add one problem in the directory.
+
+# ### Problem Statement
+# You are given a non-negative number in the form of list elements. For example, the number `123` would be provided as `arr = [1, 2, 3]`. Add one to the number and return the output in the form of a new list. 
+# 
+# **Example 1:**
+# * `input = [1, 2, 3]`
+# * `output = [1, 2, 4]`
+# 
+# 
+# **Example 2:**
+# * `input = [1, 2, 9]`
+# * `output = [1, 3, 0]`
+# 
+# **Example 3:**
+# * `input = [9, 9, 9]`
+# * `output = [1, 0, 0, 0]`
+
+
