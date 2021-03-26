cb74deb2491535ecf0382145faed7f2554ef87f1
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2021-03-25 22:40:33 -0500

    Update Add_one_2.py

--- Solved Problems/Data structures/arrays/Add_one_2.py
+++ Solved Problems/Data structures/arrays/Add_one_2.py
@@ -16,4 +16,32 @@
 # * `input = [9, 9, 9]`
 # * `output = [1, 0, 0, 0]`
 
+'''
+Logic:
+First I converted the input array of integers into a list of string 
+For example: arr = [1, 2, 3] is converted into string = ['1', '2', '3']
+Afterwards, the i used the join method to remove the "," between the string
+The string was converted into an intger and 1 was added, i.e. 123 + 1 = 124
+Finally, the total value was converted into a array.
 
+'''
+
+
+def add_one(arr):
+    '''
+    This function adds  one to the array (number)
+    and return the output in the form of a new list
+    '''
+    
+    string_arr = [str(i) for i in arr]
+    string_arr = "".join(string_arr)
+    
+    str_to_int = int(string_arr) + 1
+    int_to_str = str(str_to_int)
+    
+    new_list = [int(x) for x in int_to_str]
+    
+    
+    return new_list
+  
+  
