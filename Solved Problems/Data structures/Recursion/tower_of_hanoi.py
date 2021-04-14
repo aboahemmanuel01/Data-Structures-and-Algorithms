c52c45b06a9e2d8a72057d491fb9fcdbc204c1e2
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2021-04-13 23:32:17 -0500

    Update tower_of_hanoi.py

--- Solved Problems/Data structures/Recursion/tower_of_hanoi.py
+++ Solved Problems/Data structures/Recursion/tower_of_hanoi.py
@@ -1,1 +1,14 @@
+'''
+|| Problem Definition ||
 
+The Tower of Hanoi is a puzzle where we have three rods and n unique sized disks. The three rods are - source, destination, and auxiliary.
+
+Initally, all the n disks are present on the source rod. 
+The final objective of the puzzle is to move all disks from the source rod to the destination rod using the auxiliary rod.
+
+|| Rules of the Puzzle ||
+1. Only one disk can be moved at a time.
+2. A disk can be moved only if it is on the top of a rod.
+3. No disk can be placed on the top of a smaller disk.
+
+'''
