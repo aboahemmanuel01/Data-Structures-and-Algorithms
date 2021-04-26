093741cf33f897729bb63ab7f929358419ddb6b6
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2021-04-26 00:39:21 -0500

    Update return_code_encryption.py

--- Solved Problems/Data structures/Recursion/return_code_encryption.py
+++ Solved Problems/Data structures/Recursion/return_code_encryption.py
@@ -1,1 +1,16 @@
+'''
+Problem Definition:
+
+In an encryption system where ASCII lower case letters represent numbers in the pattern a=1, b=2, c=3... and so on, 
+this code finds out all the encrption that are possible for a given input number.
+
+For example:
+number = 123
+codes_possible = ["aw", "abc", "lc"]
+
+
+Assumption: Input number will never contain any 0s
+
+'''
+
 
