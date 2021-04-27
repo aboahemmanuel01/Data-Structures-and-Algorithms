790f177362bedb636c946504ce929dbc7c08cbe6
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2021-04-26 22:57:54 -0500

    Update return_subsets_of_array.py

--- Solved Problems/Data structures/Recursion/return_subsets_of_array.py
+++ Solved Problems/Data structures/Recursion/return_subsets_of_array.py
@@ -1,1 +1,22 @@
+'''
+Problem Definition
+
+Given an integer array, this algorithm finds and return all the subsets of the array. 
+The order of subsets in the output array is not important and hence wasn't considered. 
+However, the order of elements in a particular subset remains the same as in the input array.
+
+For example:
+
+arr = [9, 9]
+
+output = [[],
+          [9],
+          [9],
+          [9, 9]]
+          
+'''
+
+
+
+
 
