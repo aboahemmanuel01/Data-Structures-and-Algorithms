fb7415b33d07a94dab7ad56f965d67386c17fb9d
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2021-04-26 23:33:42 -0500

    Update return_subsets_of_array.py

--- Solved Problems/Data structures/Recursion/return_subsets_of_array.py
+++ Solved Problems/Data structures/Recursion/return_subsets_of_array.py
@@ -16,7 +16,71 @@ output = [[],
           
 '''
 
+def subsets(arr):
+    """
+    :param: arr - input integer array
+    Return - list of lists (two dimensional array) where each list represents a subset
+    """
+    return return_subsets(arr, 0)
 
 
+def return_subsets(arr, idx):
+    
+    # base case
+    if idx >= len(arr):
+        return [[]]
+    
+    # recursion      
+    initial_subset = return_subsets(arr, idx+1)
+    
+    results = []
+    # append existing subsets
+    for num in initial_subset:
+        results.append(num)
+        print(results)
+    
+    for num in initial_subset:
+        curr = []
+        curr.append(arr[idx])
+        curr.extend(num)
+        results.append(curr)
+        
+    return results
 
 
+# Test cases
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+    
+    output = subsets(arr)
+        
+    output.sort()
+    solution.sort()
+    
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")    
+
+
+arr = [9]
+solution = [[], [9]]
+
+test_case = [arr, solution]
+test_function(test_case)
+
+'''
+# Other test cases
+# case 2
+arr = [5, 7]
+solution = [[], [7], [5], [5, 7]]
+test_case = [arr, solution]
+test_function(test_case)
+
+# case 3
+arr = [9, 12, 15]
+solution = [[], [15], [12], [12, 15], [9], [9, 15], [9, 12], [9, 12, 15]]
+
+test_case = [arr, solution]
+test_function(test_case)
