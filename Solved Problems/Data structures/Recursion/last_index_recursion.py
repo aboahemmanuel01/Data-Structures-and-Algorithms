13b9718a3f4eb5112c702b5abb90faf748df2331
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2021-04-27 02:56:32 -0500

    Update last_index_recursion.py

--- Solved Problems/Data structures/Recursion/last_index_recursion.py
+++ Solved Problems/Data structures/Recursion/last_index_recursion.py
@@ -1,1 +1,14 @@
+'''
+Problem Definition
+
+Given an array arr and a target element target, this code finds the last index of occurence of target in arr using recursion. 
+If target is not present in arr, it returns -1.
+
+For example:
+For arr = [1, 2, 5, 5, 1, 2, 5, 4] and target = 5, output = 6
+For arr = [1, 2, 5, 5, 1, 2, 5, 4] and target = 7, output = -1
+
+'''
+
+
 
