6cd7c7a02996a055a223be848b57511749fe7567
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2021-04-28 04:25:02 -0500

    Update last_index_recursion.py

--- Solved Problems/Data structures/Recursion/last_index_recursion.py
+++ Solved Problems/Data structures/Recursion/last_index_recursion.py
@@ -10,5 +10,43 @@ For arr = [1, 2, 5, 5, 1, 2, 5, 4] and target = 7, output = -1
 
 '''
 
+def last_index(arr, target):
+    """
+    :param: arr - input array
+    :param: target - integer element
+    return: int - last index of target in arr
+    """
+    return last_index_recusion(arr, target, len(arr)-1)
 
+def last_index_recusion(arr, target, index):
+    # base case
+    if index < 0:
+        return -1
+    
+    # check to see if target has been found
+    if arr[index] == target:
+        return index
+    
+    # else make a recursive call to the rest of the array
+    return last_index_recusion(arr, target, index-1)
 
+  
+# Test cases
+def test_function(test_case):
+    arr = test_case[0]
+    target = test_case[1]
+    solution = test_case[2]
+    output = last_index(arr, target)
+    if output == solution:
+        print("Pass")
+    else:
+        print("FAIL: Expected", solution, ", but you've got:", output)
+
+# case 1
+arr = [1, 2, 5, 5, 4]
+target = 5
+solution = 3
+
+test_case = [arr, target, solution]
+test_function(test_case)        
+ 
